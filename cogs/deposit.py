09d2cc9c5504082b8343688245f256fb1b870397
Himself <32233256+himse1f@users.noreply.github.com>
2018-06-23 14:22:43 -0600

    bws update

--- cogs/deposit.py
+++ cogs/deposit.py
@@ -15,7 +15,9 @@ class Deposit:
         # Check if user exists in db
         mysql.check_for_user(user.id)
         user_addy = mysql.get_address(user.id)
-        await self.bot.say(user.mention + "'s Deposit Address: `" + str(user_addy) + "`" + "\n\nRemember to use !balance to check your balance and not an explorer. The address balance and your actual balance are not always the same!\n\n:warning: DISCLAIMER: This is BETA software! Do not send large amounts of BWS! The developers are not reliable for any lost BWS! Do not store your coins on this server - they are not backed up and can be lost at any point! You have been warned! :warning:")
+        await self.bot.say(user.mention + "'s Deposit Address: `" + str(user_addy) + "`" + "\n\nRemember to use bws.balance to check your balance and not an explorer. The address balance and your actual balance are not always the same!\n\n:warning: DISCLAIMER: This is BETA software! Do not send large amounts of BWS! The developers are not reliable for any lost BWS!\n\n REMINDER: Do not store your coins on this server - they are not backed up and can be lost at any point! You have been warned! :warning:")
 
 def setup(bot):
     bot.add_cog(Deposit(bot))
+
+
